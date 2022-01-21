118a32cff2b1c37f23968991c0a4a776c0845633
Aleks <aleksisafk@gmail.com>
2022-01-21 17:51:55 +0300

    handling forms

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,17 +3,38 @@ import React, {Component} from "react";
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
     render() {
-
-        const {onItemAdded} = this.props;
-
         return (
-            <div className='item-add-form'>
-                <button className='btn btn-outline-secondary'
-                        onClick={() => onItemAdded('Sample text')}>
+            <form className='item-add-form d-flex'
+                  onSubmit={this.onSubmit}>
+                <input type='text'
+                       className='form-control'
+                       placeholder='What needs to be done'
+                       value={this.state.label}
+                       onChange={this.onLabelChange}/>
+                <button className='btn btn-outline-secondary'>
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     };
 }
